6e8c528edae0b355fc876df5c36f66eb9054ec13
3-4-3 <xolrak5@gmail.com>
2013-09-18 16:27:38 +0200

    Updated parseBoard method

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,8 @@ public class Main {
 	private static char[][] board; //Easier access instead of string
 	private Map<State,Integer> visitedStates;
 	
+	private static int lenghtMax; // max number of columns
+	
 	public static void main(String[] args) throws IOException {
 		Vector<String> b = new Vector<String>();
 		
@@ -27,10 +29,13 @@ public class Main {
 		
 		String line;	
 		
-		
+		lenghtMax=0;
 		while(br.ready()) {
 			line = br.readLine();
 			b.add(line);
+			
+			if(lenghtMax < line.length())
+				lenghtMax = line.length();
 		} // End while
 	} // main
 	
@@ -88,8 +93,96 @@ public class Main {
 	 * Check for corners and dangerous zones
 	 * Updates the "board" (char[][]) attribute.
 	 * @param board
+	 * @author Carlos Perez
 	 */
 	public static void parseBoard(Vector<String> board){
+		Main.board = new char[board.size()][lenghtMax];
+		
+		// Normal map, exactly the same than in Vector<String> board
+		for(int row=0; row<board.size(); row++){
+			String datosF = board.get(row);
+			int maxChar = datosF.length();
+			for(int col=0; col<lenghtMax; col++){
+				if(col<maxChar) {
+					Main.board[row][col] = datosF.charAt(col);
+				} else {
+					Main.board[row][col] = ' ';
+				}
+			}
+		}
+		
+		// Put c char in corners, C if there is the player in that space. Doesn't look at the borders of the map!
+		for(int row=1; row<board.size()-1; row++){
+			for(int col=1; col<lenghtMax-1; col++){
+				// Case space
+				if(Main.board[row][col]==(' ')) {
+					// If theres one wall up or down and another wall right or left => it's a corner!
+					if((Main.board[row-1][col]==('#')||(Main.board[row+1][col]==('#')))
+							&&(Main.board[row][col-1]==('#')||(Main.board[row][col+1]==('#')))) {
+						Main.board[row][col]=('c');
+					}
+				}
+				// Case player
+				if(Main.board[row][col]==('@')) {
+					// If theres one wall up or down and another wall right or left => it's a corner!
+					if((Main.board[row-1][col]==('#')||(Main.board[row+1][col]==('#')))
+							&&(Main.board[row][col-1]==('#')||(Main.board[row][col+1]==('#')))) {
+						Main.board[row][col]=('C');
+					}
+				}
+			}
+		}
+		
+		// In case we want to compare 2 different positions, we'll put and 'x' char instead of an space
+		// if the player can be in this space without moving any box. (the player in that position would
+		// have the @ in a different position but would be the same state.
+		// This code may be moved to another method that would compare between two maps
+		ArrayList<Position> queue = new ArrayList<Position> ();
+		ArrayList<Position> visited = new ArrayList<Position> ();
+		for(int row=0; row<board.size(); row++){
+			for(int col=0; col<lenghtMax; col++){
+				if(Main.board[row][col]==('@')) {
+					queue.add(new Position(row,col));
+					visited.add(new Position(row,col));
+				}else if(Main.board[row][col]==('+')) {
+					Main.board[row][col]='.';
+					queue.add(new Position(row,col));
+					visited.add(new Position(row,col));
+				}else if(Main.board[row][col]==('C')) {
+					Main.board[row][col]='c';
+					queue.add(new Position(row,col));
+					visited.add(new Position(row,col));
+				}
+			}
+		}
+		
+		while(queue.size() != 0) {
+			Position now = queue.get(0);
+			queue.remove(0);
+			
+			// Will display an error if our player is in an invalid position, 
+			// or there is an invalid map, 'cause we'll reach the borders!
+			Position up =new Position(now.getRow()-1,now.getCol());
+			Position down =new Position(now.getRow()+1,now.getCol());
+			Position right =new Position(now.getRow(),now.getCol()+1);
+			Position left =new Position(now.getRow(),now.getCol()-1);
+			
+			Position[] pos= {up,down,right,left};
+			
+			for(Position p: pos) {
+				if(!visited.contains(p)) {
+					if(Main.board[p.getRow()][p.getCol()]==(' ')) {
+						Main.board[p.getRow()][p.getCol()] = 'x';
+						queue.add(new Position(p.getRow(),p.getCol()));
+					} else if (Main.board[p.getRow()][p.getCol()]==('.')) {
+						queue.add(new Position(p.getRow(),p.getCol()));
+					} else if (Main.board[p.getRow()][p.getCol()]==('c')) {
+						queue.add(new Position(p.getRow(),p.getCol()));
+					}
+					visited.add(new Position(p.getRow(),p.getCol()));
+				}
+			}
+		}
 		
 	}
 	
